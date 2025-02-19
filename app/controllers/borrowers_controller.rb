dc2afc6805b9db97a2bfaa06826a7ca022a37d8b
Hansel Omondi <ogopabhs@gmail.com>
2025-02-19 17:45:33 +0300

    feat: implement borrowers controller'

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -1,4 +1,46 @@
 class BorrowersController < ApplicationController
   def index
+    @borrowers = Borrower.all
+  end
+
+  def show
+    @borrower = Borrower.find(params[:id])
+  end
+
+  def new
+    @borrower = Borrower.new
+  end
+
+  def create
+    borrower = Borrower.new(borrower_params)
+    if borrower.save
+      redirect_to borrower_path(borrower), notice: "Borrower created successfully!"
+    else
+      render :new, alert: "Borrower creation failed!", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @borrower = Borrower.find(params[:id])
+  end
+
+  def update
+    borrower = Borrower.find(params[:id])
+    if borrower.update(borrower_params)
+      redirect_to borrower_path(borrower), notice: "Borrower successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    borrower = Borrower.find(params[:id])
+    borrower.destroy
+    redirect_to borrowers_path
+  end
+
+  private
+  def borrower_params
+    params.require(:borrower).permit(:name, :email, :phone)
   end
 end
