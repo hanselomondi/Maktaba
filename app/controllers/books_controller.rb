5559ddbcda70f97a2090d70df031a64f9aa82556
Hansel Omondi <ogopabhs@gmail.com>
2025-02-19 17:44:52 +0300

    feat: implement books conroller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,47 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_copies = @book.book_copies
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book), notice: "Book created successfully!"
+    else
+      render :new, alert: "Book creation failed!", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book), notice: "Book successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :author, :isbn)
   end
 end
