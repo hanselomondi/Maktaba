2fcd7920e9bbf923057cf36ab227ef31b54ce466
Hansel Omondi <ogopabhs@gmail.com>
2025-02-20 09:19:53 +0300

    Add: controller test for borrowers_controller

--- test/controllers/borrowers_controller_test.rb
+++ test/controllers/borrowers_controller_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class BorrowersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create borrower" do
+    assert_difference("Borrower.count", 1) do
+      post borrowers_path, params: { borrower: { name: "Jane Doe", email: "jane@example.com", phone: "987654321" } }
+    end
+    assert_redirected_to borrower_path(Borrower.last)
+  end
+
+  test "should not create borrower without email" do
+    post borrowers_path, params: { borrower: { name: "Jane Doe", phone: "987654321" } }
+    assert_response :unprocessable_entity
+  end
 end
