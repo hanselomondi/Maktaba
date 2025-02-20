6edb907def6a8b634cd9168a8c0b0ab9cb2aeb40
Hansel Omondi <ogopabhs@gmail.com>
2025-02-20 09:20:38 +0300

    Add: model test for borrowing_record

--- test/models/borrowing_record_test.rb
+++ test/models/borrowing_record_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class BorrowingRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @borrower = Borrower.create(name: "John Doe", email: "john@example.com", phone: "123456789")
+    @book = Book.create(title: "Ruby on Rails", author: "David Heinemeier Hansson", isbn: "1234567890")
+    @book_copy = @book.book_copies.create(serial_number: "ABC123")
+    @borrowing_record = BorrowingRecord.create(borrower: @borrower, book_copy: @book_copy, date_borrowed: Date.today)
+  end
+
+  test "should be valid with all attributes" do
+    assert @borrowing_record.valid?
+  end
+
+  test "should have a due date 14 days after borrowing" do
+    assert_equal Date.today + 14.days, @borrowing_record.due_date
+  end
+
+  test "should be marked as returned if date_returned is set" do
+    @borrowing_record.update(date_returned: Date.today)
+    assert @borrowing_record.returned?
+  end
 end
